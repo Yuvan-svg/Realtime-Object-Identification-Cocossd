0a446dbaba84d605f4b664488d2cacfff84b0083
Yuvan-svg <76049600+Yuvan-svg@users.noreply.github.com>
2021-08-01 18:53:15 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,47 @@
+status="";
+objects = [];
+function preload(){
+img = loadImage('dog_cat.jpg');
+}
+function setup(){
+canvas= createCanvas(380,380);
+canvas.center();
+video = createCapture(VIDEO);
+video.size(380,380);
+video.hide();
+objectDetector= ml5.objectDetector("cocossd", modelLoaded);
+document.getElementById("status").innerHTML="status: Detecting Objects";
+}
+function modelLoaded(){
+console.log("Model is Loaded");
+status= true;
+}
+
+function gotResults(error,results){
+if(error){
+console.log(error);
+}
+console.log(results);
+object = results;
+}
+
+function draw(){
+image(video,0,0,380,380);
+if(status !=""){
+r=random(255);
+g=random(255);
+b=random(255);
+objectDetector.detect(video,gotResult);
+for(i=0;i<object.length;i++){
+document.getElementById("status").innerHTML="Status: Objects Detected";
+document.getElementById("number_of_objects").innerHTML="Number of Objects Detected Are: "+objects.length;
+ fill(r,g,b);
+ percent= floor(objects[i].confidence*100);
+ text(objects[i].label+ " "+ percent+"%",objects[i].x+15,objects[i].y+15);
+ noFill();
+ stroke(r,g,b);
+ rect(objects[i].x,objects[i].y,objects[i].width,objects[i].height);
+}
+}
+
+}
